23d152f5abdeef8561f45b68af6c706092bb3cc6
jhy <jhy1000@gmail.com>
2023-01-23 15:16:39 +0800

    update Makefiel, and add my_case1

--- verification/bt/blk_a/cfg/tb.f
+++ verification/bt/blk_a/cfg/tb.f
@@ -1,5 +1,7 @@
 ../../../../rtl/dut.v
 -F ../env/env.f
 ../th/harness.sv
-../tc/my_test.sv
+../tc/tc_base.sv
+../tc/my_case0.sv
+../tc/my_case1.sv
 
--- verification/bt/blk_a/env/my_sequence.sv
+++ verification/bt/blk_a/env/my_sequence.sv
@@ -27,3 +27,27 @@ function my_sequence::new(string name="my_sequence");
 endfunction
 
 
+class my_sequence1 extends uvm_sequence #(my_transcation);
+    my_transcation m_trans;
+    
+    extern function new(string name="my_sequence1");
+    virtual task body();
+        if(starting_phase != null) begin
+            starting_phase.raise_objection(this);
+        end
+        repeat(10) begin
+            `uvm_do_with(m_trans, {m_trans.pload.size() == 60;})
+        end
+        #100;
+        if(starting_phase != null) begin
+            starting_phase.drop_objection(this);
+        end
+    endtask
+
+    `uvm_object_utils(my_sequence1)
+endclass
+
+function my_sequence1::new(string name="my_sequence1");
+    super.new(name);
+endfunction
+
